7f838e6ead8f9fb6a4ff76fc72c69e2203de2d3b
Geetika <babugeetika19@siesgst.ac.in>
2021-09-28 21:39:37 +0530

    commit again

--- src/main/java/seleniumexp/SeleniumExample.java
+++ src/main/java/seleniumexp/SeleniumExample.java
@@ -1,35 +1,37 @@
 package seleniumexp;
 
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.JavascriptExecutor;
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
-
 
 public class SeleniumExample {
-	
-	private WebDriver driver;
-	
-	@BeforeClass
-	public void beforeClass() {
-		driver = new ChromeDriver();
-		
-	}
-	
-	public class TestHelloworld {
-		@Test
-		public void testhelloworld()
-		{
+	@Test
+	public void testHelloWorld() {
 		System.out.println("Hey this is Geetika Babu from TE-IT, performing selenium testing.");
-		}
-		}
-
-	
-	@AfterClass
-	public void afterClass() {
-		driver.quit();
 	}
+	
+	@Test
+	public void testHelloWorld2() {
+		System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		// Launch the url
+		driver.navigate().to("http://www.javatpoint.com/");
+		
+		// Maximize the browser
+		driver.manage().window().maximize();
+		
+		// Scroll down the webpage by 5000 pixels
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		js.executeScript("scrollBy(0,10000)");
+		
+		// Click on the search text box and send value
+		driver.findElement(By.id("gsc-i-id1")).sendKeys("core java");
+		
+		// Click on the search button
+		driver.findElement(By.className("gsc-search-button")).click();
+	}
+	
 }
